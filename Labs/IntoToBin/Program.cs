edb58eb1f5847f18c8d4a3875f5d47e272d2c6c2
Sil <nomail@nimail.no>
2014-04-01 03:10:35 -0700

    #remade

---
+++ Labs/IntoToBin/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntoToBin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> test = new List<string>();
+            int input;
+
+            if (int.TryParse(Console.ReadLine(), out input))
+            {
+                Console.WriteLine();
+                while (input != 0) 
+                {                    
+                    test.Add((input % 2) == 0 ? "0" : "1");
+                    input >>= 1;
+                }
+
+                test.Reverse();
+                test.ForEach(c => Console.Write(c));
+            }
+        }
+    }
+}
---
+++ Tuts/2ndDayConApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2ndDayConApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*StringBuilder sb = new StringBuilder();
+            for (int i = 1; i <= 100; i++)
+            {
+                sb.Append(i).Append(" ");
+            }
+            
+            Console.WriteLine(sb.ToString());*/
+
+            /*string a = "Test";
+            var b = a.Clone();
+            Console.WriteLine("ref equal: {0}, oftype: {1}\nA = {2}, B = {3}", object.ReferenceEquals(a,b), b.GetType(), a ,b);*/
+            var test = Console.ReadLine();
+            Console.WriteLine(test ?? "false");
+            
+            
+        }
+    }
+}
