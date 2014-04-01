de173c416174d748fe3900c47aba8ef01a954646
Sil <nomail@nimail.no>
2014-04-01 06:06:41 -0700

    #Fix2

---
+++ Labs/LabsUnitTests/UnitTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MethsAndUnits;
+
+namespace LabsUnitTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void FindDelimTest()
+        {
+            Assert.AreEqual(Program.FindDelim(10,4), 2);
+        }
+    }
+}
---
+++ Labs/MethsAndUnits/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MethsAndUnits
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( FindDelim( 5, 3 ) );
+            Console.ReadKey();
+
+        }
+
+        public static int FindDelim( int a, int b )
+        {
+            int _dFirst, _dSecond;
+            int _result;
+
+            if ( a > b ) { Console.WriteLine( "A>B" ); _dFirst = a; _dSecond = b; }
+            else { Console.WriteLine( "B>A" ); _dFirst = b; _dSecond = a; }
+
+            _result = _dFirst % _dSecond;
+            
+            while ( _result >= 1 || _result > 0 )
+            {
+                _dFirst = _dSecond; _dSecond = _result;
+                _result = _dFirst % _dSecond;
+            }
+            return _dSecond;
+        }
+    }
+}
