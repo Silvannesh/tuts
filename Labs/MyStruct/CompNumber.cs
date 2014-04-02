98042ffc106e92ba363b37a193470d9fc58dc027
Sil <nomail@nimail.no>
2014-04-02 04:51:37 -0700

    #Lab

---
+++ Labs/MyStruct/CompNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyStruct
+{
+    public struct CompNumber
+    {
+        public double real;
+        public double abstr;
+
+        public CompNumber(int real, int abstr)
+        {
+                this.real = real;
+                this.abstr = abstr;
+        }
+    }
+
+    public static class CNMath
+    {
+        public static CompNumber Summ(this CompNumber source, CompNumber addition)
+        {
+            source.real += addition.real;
+            source.abstr += addition.abstr;
+            return source;
+        }
+    }
+}
---
+++ Labs/MyStruct/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyStruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CompNumber c1 = new CompNumber(1, 1);
+            CompNumber c2 = new CompNumber(1, 1);
+            CompNumber result = c1.Summ(c2);
+            Console.WriteLine("({0}; {1})", result.real, result.abstr);
+        }
+    }
+}
